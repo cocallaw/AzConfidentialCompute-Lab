999e51e461dcd8db801ecce000174e19aba5f0d7
Corey Callaway <coreylcallaway@gmail.com>
2022-04-06 14:41:48 -0400

    Update storage.sh

--- 99-Tools/PE-Creation/storage.sh
+++ 99-Tools/PE-Creation/storage.sh
@@ -4,13 +4,14 @@ $vnetRG = ""
 $subnetName = ""
 $paasRGName = ""
 $stgName = ""
+$stgRID = ""
 
 az storage account create --name $stgName --resource-group $paasRGName --location $region --sku Standard_LRS --kind StorageV2
 
-az network vnet subnet update --name $subnetName --resource-group $vnetRG --vnet-name $vnetName --disable-private-endpoint-network-policies true
+az network vnet subnet update --name $subnetName --resource-group $vnetRG --vnet-name $vnetName --disable-private-endpoint-network-policies false
 
 az network private-dns zone create --resource-group $vnetRG --name privatelink.blob.core.windows.net
 
 az network private-dns link vnet create --resource-group $vnetRG --virtual-network $vnetName --zone-name privatelink.blob.core.windows.net --name accblobdns --registration-enabled true
 
-az network private-endpoint create --resource-group $vnetRG --vnet-name $vnetName --subnet $subnetName --name blob-pewu --private-connection-resource-id "<AZRESOURCEID>" --group-id blob --connection-name blobconn01 --location $region
+az network private-endpoint create --resource-group $vnetRG --vnet-name $vnetName --subnet $subnetName --name blob-pewu --private-connection-resource-id $stgRID --group-id blob --connection-name blobconn01 --location $region
